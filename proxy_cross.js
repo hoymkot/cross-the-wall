5da0e5dcd2cefb633659c997ea1fc77aed6458ec
Hou Kot <chrishouwakot@gmail.com>
2021-05-03 17:47:47 -0700

    Update proxy_cross.js

--- proxy_cross.js
+++ proxy_cross.js
@@ -1,141 +1,121 @@
 const http = require('http');
 const net = require('net');
-const { URL } = require('url');
 
 // Create an HTTP tunneling proxy
 const proxy = http.createServer((req, res) => {
-  // console.log("write head 7")
-  // res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // res.end('okay');
+    // console.log("write head 7")
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end('okay');
 });
 
+function readRequest(clientSocket, scfSocket , sendReqToScf) {
+    header = ""
+    body = ""
+    content_length = 0
+    isSplitted = false
+    // raw = Buffer.from('')
+    raw = ''
+    clientSocket.on('data', function(data) {
+        raw = raw + data.toString() 
+        // raw = Buffer.concat([raw , data])
+
+        if (isSplitted == false) {
+            header = header + data.toString()
+            if (header.includes('\r\n\r\n') == true) {
+                temp = header
+                head_term_idx = temp.indexOf('\r\n\r\n')
+                header = temp.substring(0, head_term_idx)
+                body = temp.substring(head_term_idx + 4)
+                isSplitted = true
+                temp = header.substring(header.indexOf('Content-Length: ') + 16)
+                content_length = parseInt(temp.substring(0, temp.indexOf("\r\n")))
+                console.log(header)
+                console.log(body)
+                console.log(content_length)
+            }
+        } else {
+            body = body + data.toString()
+        }
+
+        if (content_length == body.length) {
+            console.log(body)
+            sendReqToScf(raw, scfSocket)
+            // full message gotten , do something use full
+        }
+    })
+
+}
 proxy.on('connect', (req, clientSocket, head) => {
     console.log("connect")
     console.log(req.url)
 
-  // Connect to an origin server
-  const { port, hostname } = new URL(`http://${req.url}`);
+    // Connect to an origin server
+    const {
+        port,
+        hostname
+    } = new URL(`http://${req.url}`);
 
-  const scf_host = 'service-jjmvpnn2-1304726915.hk.apigw.tencentcs.com'
-  const scf_port = 80
-  const scf_path = '/release/helloworld-1619933384982'
-  const scfSocket = net.connect(scf_port || 80, scf_host, () => {
-    clientSocket.write('HTTP/1.1 200 Connection Established\r\n' +
-                    'Proxy-agent: Node.js-Proxy\r\n' +
-                    '\r\n');
+    const scf_host = 'service-jjmvpnn2-1304726915.hk.apigw.tencentcs.com'
+    const scf_port = 80
+    const scf_path = '/release/helloworld-1619933384982'
+    const scfSocket = net.connect(scf_port || 80, scf_host, () => {
+        clientSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+            'Proxy-agent: Node.js-Proxy\r\n' +
+            '\r\n');
 
-    console.log("proxy accepted request")
-    scf_mock_header ='GET '+scf_path+ ' HTTP/1.1\r\n' +
-    'Host: '+ scf_host +'\r\n' +
-    // 'Connection: keep-alive\r\n' +
-    // 'Cache-Control: max-age=0\r\n' +
-    // 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"\r\n' +
-    // 'sec-ch-ua-mobile: ?0\r\n' +
-    // 'Upgrade-Insecure-Requests: 1\r\n' +
-    // 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36\r\n' +
-    // 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n' +
-    // 'Sec-Fetch-Site: none\r\n' +
-    // 'Sec-Fetch-Mode: navigate\r\n' +
-    // 'Sec-Fetch-Dest: document\r\n' +
-    // 'Accept-Encoding: gzip, deflate, br\r\n' +
-    'Accept-Language: en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7,zh-CN;q=0.6\r\n'
-    console.log(scf_mock_header)
-    scfSocket.write(scf_mock_header)
-    scfSocket.write("\r\n")
+        console.log("proxy accepted request")
 
-    console.log("sent request to SCF")
+        readRequest(clientSocket, scfSocket, function(raw, scfSocket) {
+            hex = raw
+            console.log("sent request to SCF")
+            scf_mock_header = 'POST ' + scf_path + ' HTTP/1.1\r\n' +
+                'Host: ' + scf_host + '\r\n' +
+                // TODO: may enable compression in the future 
+                'Content-Length: ' + hex.length + '\r\n'
+            // 'Accept-Encoding: gzip, deflate, br\r\n' 
+            scfSocket.write(scf_mock_header)
+            scfSocket.write("\r\n")
+            // TODO: need to encrypt the following raw string to avoid being caught by the firewall 
+            scfSocket.write(hex)
 
+        })
 
-    scf_resp_header = "" 
-    scf_resp_body = ""
-    scf_content_length = 0 
-    isSplitted = false 
-    scfSocket.on('data', function(data) {
-      if (isSplitted == false ) {
-        scf_resp_header = scf_resp_header + data.toString() 
-        if (scf_resp_header.includes('\r\n\r\n') == true ) {
-          temp = scf_resp_header 
-          head_term_idx = temp.indexOf('\r\n\r\n') 
-          scf_resp_header = temp.substring(0, head_term_idx) 
-          scf_resp_body = temp.substring(head_term_idx+4)
-          isSplitted = true
-          temp = scf_resp_header.substring( scf_resp_header.indexOf('Content-Length: ')  + 16 )
-          scf_content_length = parseInt(temp.substring(0, temp.indexOf("\r\n")) )
-          console.log(scf_resp_header)
-          console.log(scf_resp_body)
-          console.log(scf_content_length)
-        }
-      } else {
-        scf_resp_body = scf_resp_body + data.toString() 
-      }
+        // receiving data from SCF 
+        scf_resp_header = ""
+        scf_resp_body = ""
+        scf_content_length = 0
+        isSplitted = false
+        scfSocket.on('data', function(data) {
+            if (isSplitted == false) {
+                scf_resp_header = scf_resp_header + data.toString()
+                if (scf_resp_header.includes('\r\n\r\n') == true) {
+                    temp = scf_resp_header
+                    head_term_idx = temp.indexOf('\r\n\r\n')
+                    scf_resp_header = temp.substring(0, head_term_idx)
+                    scf_resp_body = temp.substring(head_term_idx + 4)
+                    isSplitted = true
+                    temp = scf_resp_header.substring(scf_resp_header.indexOf('Content-Length: ') + 16)
+                    scf_content_length = parseInt(temp.substring(0, temp.indexOf("\r\n")))
+                    console.log(scf_resp_header)
+                    console.log(scf_resp_body)
+                    console.log(scf_content_length)
+                }
+            } else {
+                scf_resp_body = scf_resp_body + data.toString()
+            }
 
-      if (scf_content_length == scf_resp_body.length) {
-          console.log(scf_resp_body)
-          // full message gotten 
-       }
-
-      // clientSocket.write(data)     
-    });
-    // clientSocket.on('data', function(data) {
-
-    //   console.log("data from client " + data.toString())
-    //   scfSocket.write(data)     
-    // });
-
-
-  })
-
+            if (scf_content_length == scf_resp_body.length) {
+                console.log(scf_resp_body)
+                // full message gotten , do something use full
+            }
+        });
+    })
 });
 
-
-
-  //   clientScoket.pipe(scfSocket)
-  //   // console.log(head)
-  //   // TODO: check if this line is necessary
-  //   // serverSocket.write(head);
-
-  //   // serverSocket.pipe(clientSocket);
-  //   // clientSocket.pipe(serverSocket);
-  // });
-
-
-  // const scf_req = http.request(options, function(response){
-  //   var str = '';
-
-  //   //another chunk of data has been received, so append it to `str`
-  //   response.on('data', function (chunk) {
-  //     str += chunk;
-  //   });
-
-  //   //the whole response has been received, so we just print it out here
-  //   response.on('end', function () {
-  //     console.log(str);
-  //   });
-  // })
-
-  //  clientSocket.write('HTTP/1.1 200 Connection Established\r\n' +
-  //                   'Proxy-agent: Node.js-Proxy\r\n' +
-  //                   '\r\n');
-
-  //   clientSocket.on('data', function(data) {
-  //     scf_req.write(data)
-  //   });
-
-
-  // scf_req.end();
-
-
-  // const serverSocket = net.connect(port || 80, hostname, () => {
-  //   // let the brower knows that this proxy accepted the request
-  //   serverSocket.write(head);
-  //   serverSocket.pipe(clientSocket);
-  //   clientSocket.pipe(serverSocket);
-  // });
 
 
 // Now that proxy is running
 proxy.listen(1337, '127.0.0.1', () => {
     console.log("listen ")
-
 
 });
