13768cdd372d6eb1043d3a05487e85754caa08a4
Hou Kot <chrishouwakot@gmail.com>
2021-05-03 20:43:48 -0700

    Update tencent_scf_impl.js
    skeleton , may not work at all

--- tencent_scf_impl.js
+++ tencent_scf_impl.js
@@ -1,10 +1,27 @@
 'use strict';
 
+
+var net = require('net');
+
 exports.main_handler = async (event, context,callback ) => {
+  const { port, hostname } = new URL(`http://${req.url}`);
+  const serverSocket = net.connect(port || 80, hostname, () => {
+    clientSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+                    'Proxy-agent: Node.js-Proxy\r\n' +
+                    '\r\n');
+    console.log(head)
+    // TODO: check if this line is necessary
+    // serverSocket.write(head);
+    serverSocket.pipe(clientSocket);
+    clientSocket.pipe(serverSocket);
+  });
+
+
+
   return {
     isBase64Encoded: false,
     statusCode: 200,
     headers: { 'Content-Type': 'text/html' },
-    body: JSON.stringify(event)
+    body: Buffer.from(event.body, 'base64').toString()
   }
 }
