1e41b36e81709dd0322deb71dd067a870c09fb59
Hou Kot <chrishouwakot@gmail.com>
2021-05-05 20:09:07 -0700

    Update local_proxy.js
    it is working now
    need to review the code and work on error handling

--- attempt 2/local_proxy.js
+++ attempt 2/local_proxy.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 const http = require('http')
 const net = require('net');
 const { URL } = require('url');
@@ -16,11 +16,11 @@ const scf_target_listener = net.createServer({}, (scf_target_socket) => {
   // 'connection' listener.
   var uuid = Buffer.from('')
   const uuid_bytes_length = Buffer.from('6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b').length
-  true_uuid = false
-  clientSocket = false
+  var true_uuid = false
+  var clientSocket = false
   scf_target_socket.on('data', (data)=>{
     if (true_uuid == false) {
-      uuid = Buffer.concat(uuid, data)
+      uuid = Buffer.concat([uuid, data])
       if (uuid.length >= uuid_bytes_length ) {
         true_uuid = uuid.slice(0, uuid_bytes_length).toString()
         data = uuid.slice(uuid_bytes_length)
@@ -50,9 +50,6 @@ scf_target_listener.on('error', (err) => {
 
 scf_target_listener.listen(8124, () => {});
 
-
-
-
 // Create an HTTP tunneling proxy
 proxy.on('connect', (req, clientSocket, head) => {
     // accepted a connection from a browser
@@ -65,7 +62,7 @@ proxy.on('connect', (req, clientSocket, head) => {
 
   var connection_info = {
     proxy_hostname: 'localhost',
-    proxy_port: 1559,
+    proxy_port: 8124, // todo there is another reference at scf_target_listener
     target_host_name: hostname,
     target_port: port,
     uuid: uuid.v4()
