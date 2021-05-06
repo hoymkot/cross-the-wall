d603f2e1ccae26d9e17d55bb4fc048993b9d3fd9
Hou Kot <chrishouwakot@gmail.com>
2021-05-05 22:44:18 -0700

    better error handling and comment

--- attempt 2/coordinator.js
+++ attempt 2/coordinator.js
@@ -6,13 +6,13 @@ const {
     URL
 } = require('url');
 
-// Create an HTTP tunneling proxy
+// create a server that accept callback info and target info from local proxy 
 const proxy = http.createServer((req, res) => {
     if (req.method === "GET") {
         res.writeHead(400, {});
         res.end()
     } else if (req.method === "POST") {
-
+        // body should hold callback info and target info from local proxy
         var body = "";
         req.on("data", function(chunk) {
             body += chunk;
@@ -20,32 +20,41 @@ const proxy = http.createServer((req, res) => {
 
         req.on("end", function() {
             var target_connection_info = JSON.parse(body.toString())
+            console.log("info", new Date().toISOString(), "target_connection_info", body.toString())
 
-            var connect_socket = async () => {
 
-                var proxyPromise = new Promise((resolve, reject) => {
-                    var proxySocket = net.connect(target_connection_info['proxy_port'] || 80, target_connection_info['proxy_hostname'] , () => {
-                        resolve(proxySocket)
-                        // TODO:error handling
-                    })
+            var proxyPromise = new Promise((resolve, reject) => {
+                var proxySocket = net.connect(target_connection_info['proxy_port'] || 80, target_connection_info['proxy_hostname'], () => {
+                    resolve(proxySocket)
+                    // TODO:error handling
+                })
+                proxySocket.on("error", (err) => {
+                    console.log("info", new Date().toISOString(), "proxySocket", err)
+                    reject(err)
+                })
+            })
+
+
+            var targetPromise = new Promise((resolve, reject) => {
+                var targetSocket = net.connect(target_connection_info['target_port'] || 80, target_connection_info['target_host_name'], () => {
+                    resolve(targetSocket)
+                    // TODO:error handling
+                })
+                targetSocket.on("error", (err) => {
+                    console.log("info", new Date().toISOString(), "targetSocket", err)
+                    reject(err)
+                })
+            })
+
+            Promise.all([targetPromise, proxyPromise])
+                .then((sockets) => {
+                    sockets[1].write(target_connection_info.uuid)
+                    sockets[0].pipe(sockets[1])
+                    sockets[1].pipe(sockets[0])
+                }).catch((err) => {
+                    console.log("warning", new Date().toISOString(), "[targetPromise, proxyPromise]", "unable to bridge")
                 })
 
-
-                var targetPromise = new Promise((resolve, reject) => {
-                    var targetSocket = net.connect(target_connection_info['target_port']  || 80, target_connection_info['target_host_name'] , () => {
-                        resolve(targetSocket)
-                        // TODO:error handling
-                    })
-                })
-
-                var sockets = await Promise.all([targetPromise, proxyPromise])
-                sockets[1].write(target_connection_info.uuid)
-                sockets[0].pipe(sockets[1])
-                sockets[1].pipe(sockets[0])
-                // todo what if one fail to connect
-
-            }
-            connect_socket()
         });
     }
 });
@@ -53,5 +62,5 @@ const proxy = http.createServer((req, res) => {
 
 // Now that proxy is running
 proxy.listen(80, '127.0.0.1', () => {
-    console.log("listen ")
+    console.log("coordinator started ")
 });
