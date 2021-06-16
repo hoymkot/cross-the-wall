32aa4db68f840300a8ddc85bf9ac86408aeb2a59
Hou Kot <chrishouwakot@gmail.com>
2021-06-16 16:42:34 -0700

    remove branch

--- Firewall_IDS_Enabled/coordinator/config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = {
-	COORDINATOR_HOSTNAME : 'localhost',
-	COORDINATOR_PORT : 8081,
-	RETURN_IP_PORT_PORT:8080, 
-	KEY_FILE : 'key.pem',
-	CERT_FILE : 'cert.pem'
-}
--- Firewall_IDS_Enabled/return_ip_port/config.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-module.exports = {
- "port" : 8080, // the listening port 
-}
